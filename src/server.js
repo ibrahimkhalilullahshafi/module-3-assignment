34e77f00c782606e72ff60396d139ddb5dfd18a6
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2025-03-20 21:42:30 +0600

    server.js output msg change

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello World from Shafi' });
+  res.json({ message: 'Hello World' });
 });
 
 let server;
